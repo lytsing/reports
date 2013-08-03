e77ee8557c553dfecda1cacdd23c3aec39846a3d
Lytsing Huang <hlqing@gmail.com>
2013-08-03 23:49:46 +0800

    move app/assets/javascripts/active_admin.js.coffee into vendor/assets/javascripts/active_admin.js.coffee

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,7 +6,7 @@ class PlansController < ApplicationController
   # GET /plans
   # GET /plans.json
   def index
-    @plans = Plan.all
+    @plans = current_user.plans.page(params[:page]).per(4)    
   end
 
   # GET /plans/1
